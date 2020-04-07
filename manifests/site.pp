23a7d1fd4c4365e681fc551177227067700a4e0a
Reilly Schreiber <reschrei@users.noreply.github.com>
2020-04-07 13:49:08 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,16 @@
 node default {
+  file {
+    ensure => file,
+    content => 'This is a readme',
+    owner => 'root',
+  }
 }
 node 'master.puppet.vm' {
   include role::master_server
+}
+node /^web/ {
+  include roll::app_server
+}
+node /^db/ {
+  include roll::db_server
 }
